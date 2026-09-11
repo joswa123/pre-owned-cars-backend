eaac9981ebf3a073c920d16bd35b4d5ef28d4d9e
Thiyagu <thiyagu@minmegam.com>
2026-09-11 12:37:46 +0530

    feat: add notification routes for retrieving and marking messages as read

--- src/routes/v1/notificationRoutes.js
+++ src/routes/v1/notificationRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const notificationController = require('../../controllers/notificationController');
-const { protect } = require('../../middlewares/authMiddleware');
+const { protect } = require('../../middlewares/auth');
 
 router.use(protect);
 
